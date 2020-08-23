945fca92cc64dc0ae760350c5e41c5ab35a40a2a
nuraiusa <nurayberdalieva@gmail.com>
2020-08-23 23:44:51 +0000

    added subnets

--- homwork/rout.table.tf
+++ homwork/rout.table.tf
@@ -8,6 +8,19 @@ resource "aws_route_table" "rt" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.main.id}"
+  subnet_id      = "${aws_subnet.public1.id}"
+  route_table_id = "${aws_route_table.rt.id}"
+
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = "${aws_subnet.public2.id}"
   route_table_id = "${aws_route_table.rt.id}"
 }
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = "${aws_subnet.public3.id}"
+  route_table_id = "${aws_route_table.rt.id}"
+}
+
+
